ea64e8d72f2e6c5889b7ba7be4000e20c5389d9c
nguyen phuong <nguyenphuong.ktqt@gmail.com>
2020-07-03 14:39:35 +0700

    Set the routes contraints for the api

--- config/application.rb
+++ config/application.rb
@@ -14,12 +14,24 @@ require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require(:default, Rails.env)
 
 module MarketPlaceApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+      g.view_specs false
+      g.helper_specs false
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper = false
+    end
+  
+    config.autoload_paths += %W(\#{config.root}/lib)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
-Rails.application.routes.draw do
+require 'api_constraints'
+
+MarketPlaceApi::Application.routes.draw do
   # Api definition
-  namespace :api, default: {format: :json},
-              constraints: { subdomain: 'api'}, path: '/' do 
-    # We are going to list our resources here
-    
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+    end
   end
 end
